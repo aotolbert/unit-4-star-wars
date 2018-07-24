096f57d451ef6de8fcc6f2ae842019bd81701e35
aotolbert@gmail.com <aotolbert@gmail.com>
2018-07-23 23:59:58 -0400

    Skeleton sans logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function() {
+
+    var obiwan = $("#obiwan");
+    var hansolo = $("#hansolo");
+    var kyloren = $("#kyloren");
+    var stormtrooper = $("#stormtrooper");
+
+    // $("#obiwan").on("click", function() {
+    //     obiwan.hide({ top: "+=200px" }, "normal");
+    //     console.log("clicked");
+    // });
+
+    $("#rad").on("click", function() {
+        obiwan.hide({ top: "+=200px" }, "normal");
+        console.log("clicked 2");
+    });
+
+
+
+
+});
+
+
+
